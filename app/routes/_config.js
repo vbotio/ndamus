7c4742790798ac542c8f8efb41e5a6ff76b19037
Kaio <kaio.leal@bluecore.it>
2016-12-06 23:21:52 -0200

    allow cors

--- app/routes/_config.js
+++ app/routes/_config.js
@@ -8,6 +8,15 @@ router.use(function(req, res, next) {
     next(); // make sure we go to the next routes and don't stop here
 });
 
+// middleware to allow cors
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
+
+
 router.use('/bear', require('./bear.js'));
 router.use('/guess', require('./guess.js'));
 
